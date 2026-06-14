fd8bb69ec83eed1eba7a6326fc151ddffa82bb29
gabrieldevreis <gabrielreisdev@gmail.com>
2026-06-14 14:21:07 -0300

    Add routing for Goals, Investments, and Transactions components in MainRouter

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { Login } from "../../pages/login";
 import { Dashboard } from "../../pages/dashboard";
+import { Goals } from "../../pages/goals";
+import { Investments } from "../../pages/investments";
+import { Transactions } from "../../pages/transactions";
 
 export function MainRouter() {
   return (
@@ -10,6 +13,9 @@ export function MainRouter() {
          <Route path="/" element={<Navigate to="/login" />} />
          <Route path="/login" element={<Login />} />
          <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/goals" element={<Goals />} />
+         <Route path="/investments" element={<Investments />} />
+         <Route path="/transactions" element={<Transactions />} />
       </Routes>
   </BrowserRouter>
   );

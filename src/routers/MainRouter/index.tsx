e8fa00831af6fe9b76e23ab909cd82b267e457fd
gabrieldevreis <gabrielreisdev@gmail.com>
2026-06-15 21:50:27 -0300

    Refactor MainRouter to include MainTemplate for nested routing of Dashboard, Goals, Investments, and Transactions components

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -5,18 +5,22 @@ import { Dashboard } from "../../pages/dashboard";
 import { Goals } from "../../pages/goals";
 import { Investments } from "../../pages/investments";
 import { Transactions } from "../../pages/transactions";
+import { MainTemplate } from "../../templates/MainTemplate";
 
 export function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/" element={<Navigate to="/login" />} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/dashboard" element={<Dashboard />} />
-         <Route path="/goals" element={<Goals />} />
-         <Route path="/investments" element={<Investments />} />
-         <Route path="/transactions" element={<Transactions />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        
+        <Route element={<MainTemplate />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route path="/investments" element={<Investments />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Route>
       </Routes>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 }
